5ae4c8b7937b3eee634cb5b65fd203c0e7a7e019
GuilhermeHenrique659 <guilhermehenrique.o@hotmail.com>
2022-04-25 08:53:05 -0300

    correcao da colisao de salas chat

--- controllers/chatcontrollers.py
+++ controllers/chatcontrollers.py
@@ -3,29 +3,29 @@ from flask_socketio import emit,send, join_room,leave_room
 from flask import render_template, session,request
 from factoryDao import dao
 
-
-id_friend = None
-
 class ControllerChat:
 
     @server.loggin_required
     def chat(self,id):
         user_chat = dao.user.search_user_profile(id)
-        return render_template('chat.html',id_friend=id, friend = user_chat)
+        friend_hash = hash(user_chat._email)
+        user_hash = hash(session['login_user'])
+        room_hash = user_hash + friend_hash
+        return render_template('chat.html',room_hash=room_hash, friend = user_chat)
 
     @server.socketio.on('join')
     def on_join(data):
         username = data['username']
         room = data['channel']
         join_room(room)
-        send(' has entered the room.', to=room)
+        send(username +' has entered the room.', to=room)
 
     @server.socketio.on('leave')
     def on_leave(data):
         username = data['username']
-        room = data['room']
+        room = data['channel']
         leave_room(room)
-        send(' has left the room.', to=room)
+        send(username + ' has left the room.', to=room)
 
     @server.socketio.on("send message")
     def message(data):
