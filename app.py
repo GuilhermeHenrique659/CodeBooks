fc46a0d65012da3ec7e9586032fd083e5e906e1f
GuilhermeHenrique659 <guilhermehenrique.o@hotmail.com>
2022-05-17 15:37:34 -0300

    procfile app test

---
+++ app.py
@@ -0,0 +1,5 @@
+from config import server
+from route import Routes
+
+routes = Routes()
+server.run()
--- main.py
+++
@@ -1,5 +0,0 @@
-from config import server
-from route import Routes
-
-routes = Routes()
-server.run()
