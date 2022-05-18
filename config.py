0b702a6716f86785eff59b2dd15d5d1fa622124e
GuilhermeHenrique659 <guilhermehenrique.o@hotmail.com>
2022-05-18 09:13:57 -0300

    try deploy 25

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ class Server:
         return dictionary
 
     def run(self):
-        self.__socketio.run(self.__app)
+        self.__app.run()
 
     def loggin_required(self, controller):
         def wrapper(*agrs, **kwargs):
